9c8c258e4fcb8fae7ff99231f560a6707daa1aae
Denis Chapligin <me@chollya.org>
2016-08-15 14:57:51 +0300

    Tuned HID descriptot

--- fw/Inc/usbd_conf.h
+++ fw/Inc/usbd_conf.h
@@ -72,7 +72,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     4
 /*---------- -----------*/
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     38
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     28
 /****************************************/
 /* #define for FS and HS identification */
 #define DEVICE_FS 		0
